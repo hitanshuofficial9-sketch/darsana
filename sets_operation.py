ec3c8ba2194ade5590b7a04530a4bd02a4602fd8
darsana <darsana@gmail.com>
2026-01-23 12:08:55 +0530

    operation in sets

---
+++ sets_operation.py
@@ -0,0 +1,15 @@
+a={1,2,3,4}
+b={3,4,5,6}
+print(a|b)
+print(a-b)
+print(a^b)
+print(a&b)
+
+x={1,2}
+y={1,2,3,4,5}
+print(x.issubset(y))
+print(y.issubset(x))
+print(x<=y)
+print(y<=x)
+print(x>=y)
+print(y>=x)
