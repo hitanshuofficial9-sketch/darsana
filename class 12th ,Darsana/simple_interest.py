acc8aa64cd79a5551d8bb59729c98ff9a9648968
darsana <darsana@gmail.com>
2026-01-23 12:26:39 +0530

    simple interest

---
+++ class 12th ,Darsana/simple_interest.py
@@ -0,0 +1,7 @@
+def simpleinterest():
+    p=int(input("enter your princple amount"))
+    r=int(input("enter your rate of interest"))
+    t=int(input("enter time in month"))
+    print((p*r*t)/(100*12))
+
+simpleinterest()
