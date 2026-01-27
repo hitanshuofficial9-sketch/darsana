1034452c886eacde85c870f0d610173f0a4329e6
darsana <darsana@gmail.com>
2026-01-27 12:15:45 +0530

    pivot index

--- list_pivot_index.py
+++ list_pivot_index.py
@@ -1,6 +1,12 @@
-l=[1,7,3,6,5,6]
-leftsum=0
-rightsum=0
+l=[1,2,4,5]
+x=[]
 for i in range(0,len(l)):
-    leftsum+rightsum
-print(i)
+    left_sum=0
+    right_sum=0
+    for j in range(i+1,len(l)):
+        right=right_sum+l[j]
+    for k in range(0,i-1):
+        left_sum=left_sum+l[k]
+    if left_sum==right_sum:
+        print(i)
+        break
