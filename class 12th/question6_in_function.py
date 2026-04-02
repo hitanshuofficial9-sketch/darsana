b20e4d1a3bc4a361027a071eb62f330355ed5d0d
darsana <darsana@gmail.com>
2026-04-02 11:17:34 +0530

    question6

---
+++ class 12th/question6_in_function.py
@@ -0,0 +1,14 @@
+def listchange(l):
+    for i in range(len(l)):
+        if l[i]%2==0:
+            l[i]=l[i]*2
+
+        if l[i]%3==0:
+            l[i]=l[i]*3
+        else:
+            l[i]=l[i]*5
+
+l=[2,6,9,10]
+listchange(l)
+for i in l:
+    print(i,end="#")
